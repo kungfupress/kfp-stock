f46f1028a773030f679684c73f5e713d9195245d
tonioruiz <kungfupress@gmail.com>
2019-07-18 19:36:46 +0200

    Cambia envío de formulario para que use admin_post

--- admin.php
+++ admin.php
@@ -1,8 +1,13 @@
 <?php
 /** Archivo: admin.php */
 
+define('KFP_STOCK_MENU_SLUG', 'kfp_stock');
+
 // Aquí comienza la parte administrativa del plugin
 add_action("admin_menu", "Kfp_Stock_menu");
+add_action("admin_post_kfp-stock-comprar", "Kfp_Stock_Procesa_Form_compra");
+add_action("admin_post_kfp-stock-vender", "Kfp_Stock_Procesa_Form_venta");
+add_action("admin_notices", "kfp_stock_admin_notices");
 
 /**
  * Agrega el menú del plugin al panel de administración
@@ -12,7 +17,7 @@ add_action("admin_menu", "Kfp_Stock_menu");
 function Kfp_Stock_menu() 
 {
     add_menu_page(
-        'Stock', 'Stock', 'manage_options', 'kfp_stock_menu', 'Kfp_Stock_admin', 
+        'Stock', 'Stock', 'manage_options', KFP_STOCK_MENU_SLUG, 'Kfp_Stock_admin', 
         'dashicons-feedback'
     );
 }
@@ -26,10 +31,9 @@ function Kfp_Stock_admin()
 {
     echo '<div class="wrap"><h1>Stock de productos</h1>';
     Kfp_Stock_Tabla_productos();
-    echo Kfp_Stock_Admin_Form_comprar();
-    echo Kfp_Stock_Admin_Form_vender();
+    echo Kfp_Stock_Form_comprar();
+    echo Kfp_Stock_Form_vender();
     echo '</div>';
-    // Agrega el script
     wp_enqueue_script('kfp-stock-script', plugins_url('script.js', __FILE__));
 }
 
@@ -59,26 +63,10 @@ function Kfp_Stock_Tabla_productos()
  *
  * @return void
  */
-function Kfp_Stock_Admin_Form_comprar()
+function Kfp_Stock_Form_comprar()
 {
     global $wpdb;
     $tabla_producto = $wpdb->prefix . 'producto';
-    //Si viene un envío del formulario graba los cambios en la tabla
-    if (!empty($_POST) 
-        && $_POST['accion'] == 'comprar' 
-        && $_POST['cantidad'] > 0 
-        && $_POST['id_producto'] != ''
-    ) {
-        $cantidad_comprada = (int)$_POST['cantidad'];
-        $id_producto = (int)$_POST['id_producto'];
-        $wpdb->query(
-            $wpdb->prepare(
-                "UPDATE `$tabla_producto` SET cantidad = cantidad + %d 
-                    WHERE id = %d",
-                $cantidad_comprada, $id_producto
-            )
-        ); 
-    }
     // Recupera los productos para el select del formulario
     $productos = $wpdb->get_results(
         "SELECT * from $tabla_producto ORDER BY nombre"
@@ -86,8 +74,9 @@ function Kfp_Stock_Admin_Form_comprar()
     ob_start();
     ?>
     <h2>Comprar productos</h2>
-    <form action="" method="post" id="kfp-stock-form-comprar">
-        <input type="hidden" name="accion" value="comprar">
+    <form action="<?php echo esc_url(admin_url('admin-post.php')); ?>" 
+        method="post" id="kfp-stock-form-comprar">
+        <input type="hidden" name="action" value="kfp-stock-comprar">
         <p>
             <label for="producto">Producto</label>
             <select name="id_producto" id="kfp-stock-compra-producto" required>
@@ -118,29 +107,10 @@ function Kfp_Stock_Admin_Form_comprar()
  *
  * @return void
  */
-function Kfp_Stock_Admin_Form_vender()
+function Kfp_Stock_Form_vender()
 {
     global $wpdb;
-    $tabla_producto = $wpdb->prefix . 'producto';
-    
-    //Si viene un envío del formulario graba los cambios en la tabla
-    if (!empty($_POST) 
-        && $_POST['accion'] == 'vender' 
-        && $_POST['cantidad'] > 0 
-        && $_POST['id_producto'] != ''
-    ) {
-        $cantidad_vendida = (int)$_POST['cantidad'];
-        $id_producto = (int)$_POST['id_producto'];
-        $consulta_cantidad_actual = "SELECT cantidad FROM $tabla_producto 
-            WHERE id = $id_producto"; 
-        $cantidad_actual = $wpdb->get_var($consulta_cantidad_actual);
-        $cantidad_final = $cantidad_actual - $cantidad_vendida;
-        $wpdb->update(
-            $tabla_producto, 
-            array( 'cantidad' => $cantidad_final ), 
-            array( 'id' => $id_producto ) 
-        );
-    }
+    $tabla_producto = $wpdb->prefix . 'producto';   
     // Recupera los productos para el select del formulario si tienen stock
     $productos = $wpdb->get_results(
         "SELECT * from $tabla_producto WHERE cantidad > 0 ORDER BY nombre"
@@ -148,8 +118,9 @@ function Kfp_Stock_Admin_Form_vender()
     ob_start();
     ?>
     <h2>Vender productos</h2>
-    <form action="" method="post" id="kfp-stock-form-vender">
-        <input type="hidden" name="accion" value="vender">
+    <form action="<?php echo esc_url(admin_url('admin-post.php')); ?>" 
+        method="post" id="kfp-stock-form-vender">
+        <input type="hidden" name="action" value="kfp-stock-vender">
         <p>
             <label for="producto">Producto</label>
             <select name="id_producto" id="kfp-stock-venta-producto" required>
@@ -173,4 +144,119 @@ function Kfp_Stock_Admin_Form_vender()
     </form>
     <?php
     return ob_get_clean();
+}
+
+/**
+ * Procesa el formulario de compra
+ *
+ * @return void
+ */
+function Kfp_Stock_Procesa_Form_compra()
+{
+    global $wpdb;
+    $tabla_producto = $wpdb->prefix . 'producto';
+    //Si viene un envío del formulario graba los cambios en la tabla
+    if (!empty($_POST) 
+        && $_POST['cantidad'] > 0 
+        && $_POST['id_producto'] != ''
+    ) {
+        $cantidad_comprada = (int)$_POST['cantidad'];
+        $id_producto = (int)$_POST['id_producto'];
+        $wpdb->query(
+            $wpdb->prepare(
+                "UPDATE `$tabla_producto` SET cantidad = cantidad + %d 
+                    WHERE id = %d",
+                $cantidad_comprada, $id_producto
+            )
+        );
+        $aviso = "success";
+        $texto_aviso = "Se ha realizado una compra correctamente";
+        wp_redirect(
+            esc_url_raw(
+                add_query_arg(
+                    array(
+                        'kfp_stock_aviso' => $aviso,
+                        'kfp_stock_texto_aviso' => $texto_aviso,
+                    ),
+                    admin_url('admin.php?page='. KFP_STOCK_MENU_SLUG) 
+                ) 
+            ) 
+        );
+        exit();
+    } else {
+        wp_die();
+    }
+}
+
+/**
+ * Procesa el formulario de venta
+ * Sigue una estrategia distinta al de compra a la hora de grabar los datos
+ * para dejar documentadas dos opciones distintas
+ *
+ * @return void
+ */
+function Kfp_Stock_Procesa_Form_venta()
+{
+    global $wpdb;
+    $tabla_producto = $wpdb->prefix . 'producto';   
+    //Si viene un envío del formulario graba los cambios en la tabla
+    if (!empty($_POST) 
+        && $_POST['cantidad'] > 0 
+        && $_POST['id_producto'] != ''
+    ) {
+        $cantidad_vendida = (int)$_POST['cantidad'];
+        $id_producto = (int)$_POST['id_producto'];
+        $consulta_cantidad_actual = "SELECT cantidad FROM $tabla_producto 
+            WHERE id = $id_producto"; 
+        $cantidad_actual = $wpdb->get_var($consulta_cantidad_actual);
+        $cantidad_final = $cantidad_actual - $cantidad_vendida;
+        $wpdb->update(
+            $tabla_producto, 
+            array( 'cantidad' => $cantidad_final ), 
+            array( 'id' => $id_producto ) 
+        );
+        $aviso = "success";
+        $texto_aviso = "Se ha realizado una venta correctamente";
+        wp_redirect(
+            esc_url_raw(
+                add_query_arg(
+                    array(
+                        'kfp_stock_aviso' => $aviso,
+                        'kfp_stock_texto_aviso' => $texto_aviso,
+                    ),
+                    admin_url('admin.php?page='. KFP_STOCK_MENU_SLUG) 
+                ) 
+            ) 
+        );
+        exit();
+    } else {
+        wp_die();
+    }
+}
+
+/**
+ * Muestra los avisos de éxito o error
+ * 
+ * @return void
+ */
+function Kfp_Stock_Admin_notices() 
+{              
+    if (isset($_REQUEST['kfp_stock_aviso'])) {
+        if ($_REQUEST['kfp_stock_aviso'] === "success") {
+            $html = '<div class="notice notice-success is-dismissible">'; 
+            $html .= '<p>'; 
+            $html .= htmlspecialchars($_REQUEST['kfp_stock_texto_aviso']); 
+            $html .= '</p></div>';
+            echo $html;
+        }
+        if ($_REQUEST['kfp_stock_aviso'] === "error") {
+            $html = '<div class="notice notice-error is-dismissible">'; 
+            $html .= '<p>'; 
+            $html .= htmlspecialchars($_REQUEST['kfp_stock_texto_aviso']); 
+            $html .= '</p></div>';
+            echo $html;
+        }
+    } else {
+        return;
+    }
 }
